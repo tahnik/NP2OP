f7d9df73862b16d9612440bb62bf29d9ecafcaf5
wottonm <wottonm@visa.com>
2019-01-20 14:15:25 +0000

    adding db connection details

--- backend/api/np2op.go
+++ backend/api/np2op.go
@@ -6,7 +6,18 @@ import (
 
 var ()
 
-//ServerState holds the global variables inside the package such as the db pointer
-type ServerState struct {
-	DB *sqlx.DB
-}
+type (
+	//ServerState holds the global variables inside the package such as the db pointer
+	ServerState struct {
+		DB *sqlx.DB
+	}
+
+	//Post is an instance of a user's Post
+	Post struct {
+		ID          int    `json:"id"`
+		Username    string `json:"username"`
+		Created     string `json:"created"`
+		Description string `json:"description"`
+		Goal        int    `json:"goal"`
+	}
+)
--- backend/api/routes.go
+++ backend/api/routes.go
@@ -23,14 +23,44 @@ func (s *ServerState) NewRouter() *gin.Engine {
 	Posts := r.Group("/posts/")
 	{
 		//placeholder handler functions demonstrating the grouping of the API
-		Posts.POST("/", s.addUser) //localhost:8080
-		Posts.GET("/", s.placeholder)
-		Posts.GET("/:id", s.placeholder) //localhost:8080/user/sdakjfbdshfbsdihvb
-		Posts.PUT("/:id", s.placeholder)
-		Posts.DELETE("/:id", s.placeholder)
+		Posts.POST("/", s.addPost) //localhost:8080
+		Posts.GET("/", s.getPosts)
+		Posts.GET("/:id", s.getPost) //localhost:8080/user/sdakjfbdshfbsdihvb
+		Posts.PUT("/:id", s.updatePost)
+		Posts.DELETE("/:id", s.deletePost)
 	}
 
 	return r
+}
+
+//Placeholder function demonstrating the gin grouping
+func (s *ServerState) addPost(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+//Placeholder function demonstrating the gin grouping
+func (s *ServerState) getPosts(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+//Placeholder function demonstrating the gin grouping
+func (s *ServerState) getPost(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+//Placeholder function demonstrating the gin grouping
+func (s *ServerState) updatePost(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+//Placeholder function demonstrating the gin grouping
+func (s *ServerState) deletePost(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
 
 //Placeholder function demonstrating the gin grouping
