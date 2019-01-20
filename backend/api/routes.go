3021da4f1c8cc5c2af727bd76fe470f50621fd29
baker-james <bakerjames1995@gmail.com>
2019-01-20 23:08:59 +0000

    Implement getUser() by email

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -17,7 +17,7 @@ func (s *ServerState) NewRouter() *gin.Engine {
 		//placeholder handler functions demonstrating the grouping of the API
 		Users.POST("/", s.addUser) //localhost:8080
 		Users.GET("/", s.getUsers)
-		Users.GET("/:id", s.getUser) //localhost:8080/user/sdakjfbdshfbsdihvb
+		Users.GET("/:email", s.getUser) //localhost:8080/user/sdakjfbdshfbsdihvb
 		//Users.PUT("/:id", s.placeholder)
 		//Users.DELETE("/:id", s.placeholder)
 	}
@@ -124,17 +124,20 @@ func (s *ServerState) getUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"user": u})
 }
 
+//
 func (s *ServerState) getUser(c *gin.Context) {
-	var u User
-	if err := c.ShouldBind(&u); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"status": err})
+	var u []User
+	q := fmt.Sprintf("SELECT * FROM user WHERE email='%s'", c.Param("email"))
+	fmt.Println(q)
+	if err := s.DB.Select(&u, q); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false, "status": err.Error()})
+		return
 	}
-
-	if err := s.DB.Select(&u, "select "+string(u.Id)+" from users"); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"status": err})
+	if len(u) == 0 {
+		c.JSON(http.StatusOK, gin.H{"ok": false, "status": "No user by that name"})
+		return
 	}
-
-	c.JSON(http.StatusOK, gin.H{"user": u})
+	c.JSON(http.StatusOK, gin.H{"ok": true})
 }
 
 // curl --request POST --data '{"name":"james","email":"j@j.com", "Phone":"07875","usertype_id":0, "country":"UK"}' "http://localhost:8080/user/"
