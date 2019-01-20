8d0e022afdd80387356e813233502c6168eb6f3d
wottonm <wottonm@visa.com>
2019-01-20 13:40:58 +0000

    add placeholder db connection info

--- backend/main.go
+++ backend/main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 
 	"NP2OP/backend/api"
+
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
 	fmt.Println("we in bois")
-	r := api.NewRouter()
+	db := sqlx.MustConnect("mysql", "user=root dbname=ht password=root sslmode=disable")
+
+	server := api.ServerState{
+		DB: db,
+	}
+	r := server.NewRouter()
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
